0686fa20cda45d376d8e6233cc9c8c73e488a6e7
sabelom <50705774+sabelo-dev@users.noreply.github.com>
2025-06-11 03:57:14 +0200

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -30,7 +30,7 @@ const Header: React.FC = () => {
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
           <Link to="/" className="flex-shrink-0 flex items-center">
-            <span className="text-xl font-bold text-wwe-navy">WWE Store</span>
+            <span className="text-xl font-bold text-wwe-navy">WWEAPP</span>
           </Link>
 
           {/* Desktop Navigation */}
